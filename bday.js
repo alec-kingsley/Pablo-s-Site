ded11925f55f2943fcf867194c01cd776806a17d
Fisher792 <47871513+Fisher792@users.noreply.github.com>
2022-01-16 21:32:35 +0000

    Birthday canvas fades out so buttons can be accessed.

--- bday.js
+++ bday.js
@@ -1,4 +1,4 @@
-//code from codemyui.com/confetti-falling-background-using-canvas
+//code mostly from codemyui.com/confetti-falling-background-using-canvas
 const canvasEl = document.querySelector('#canvas');
 
 const w = canvasEl.width = window.innerWidth;
@@ -16,28 +16,22 @@ function loop() {
 
 function Confetti () {
   //construct confetti
-  const colours = ['#2a438c', '#d92332', '#f2f2f2'];
 
   this.x = Math.round(Math.random() * w);
   this.y = Math.round(Math.random() * h)-(h/2);
   this.rotation = Math.random()*360;
 
   const size = Math.random()*(w/60);
-  this.size = size < 15 ? 15 : size;
+  if (size < 15) this.size = 15;
 
-  this.color = colours[Math.floor(colours.length * Math.random())];
+  const colors = ['#2a438c', '#d92332', '#f2f2f2'];
+  this.color = colors[Math.floor(colors.length * Math.random())];
 
   this.speed = this.size/7;
   
-  this.opacity = Math.random();
+  this.opacity = Math.random()/2+0.4;
 
   this.shiftDirection = Math.random() > 0.5 ? 1 : -1;
-}
-
-Confetti.prototype.border = function() {
-  if (this.y > h) {
-    this.y = h;
-  }
 }
 
 Confetti.prototype.update = function() {
@@ -46,9 +40,7 @@ Confetti.prototype.update = function() {
   if (this.y <= h) {
     this.x += this.shiftDirection/3;
     this.rotation += this.shiftDirection*this.speed/100;
-  }
-
-  if (this.y > h) this.border();
+  } else this.y = h;
 };
 
 Confetti.prototype.draw = function() {
@@ -66,3 +58,16 @@ const confNum = Math.floor(w / 4);
 const confs = new Array(confNum).fill().map(_ => new Confetti());
 
 loop();
+
+function fadeOut() {
+
+  let vis = false;
+  confs.forEach((conf) => {
+    vis = false;
+    if (conf.opacity > 0.03) vis = true;
+    conf.opacity *= 0.995;
+  })
+  if (vis) requestAnimationFrame(fadeOut);
+  else canvasEl.remove();
+}
+setTimeout(() => {fadeOut();}, 12*h);
