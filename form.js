6fcf5624aeea8cb27669c71c41e2eec70ab7cd53
KyleTurkey <48809134+JosephStalin@users.noreply.github.com>
2022-01-22 22:39:04 +0000

    Finishing survey

--- form.js
+++ form.js
@@ -1,4 +1,4 @@
-function p(k){for(c=k.length,i=s=x=0;i<c;i++)for(e=i+s;e<c;e++){t=k.substring(i,e);l=t.length;p=1;for(a=0;a<l;a++)p=t[a]!=t[l-a-1]?0:p;if(p>0)x=i,s=e-i}alert(k.substring(x,x+s))}
+function p(k){for(c=k.length,i=s=x=0;i<c;i++)for(e=i+s;e<c;e++){t=k.substring(i,e);l=t.length;p=1;for(a=0;a<l;a++)p=t[a]!=t[l-a-1]?0:p;if(p>0)x=i,s=e-i}return k.substring(x,x+s)}
 
 // lang codes: 0 - English, 1 - Spanish, 2 - Old English
 // code modified from https://blog.milanmaharjan.com.np/post/build-a-custom-contact-form-for-your-static-website/
@@ -14,7 +14,7 @@ function validate() {
     // Email must be name of program, Message is main input,
     // subject is secondary input
     if (document.forms["contactUs"]["Email"].value == "palindrome")
-    p(document.forms["contactUs"]["Message"].value);
+    popUpGen("Output",p(document.forms["contactUs"]["Message"].value));
     return false;
   }
   let r = "";
@@ -30,14 +30,14 @@ function validate() {
   }
   if (r != "") {
     msg = [r+" must be filled out","Debe llenar "+r];
-    alert(msg[lang]);
+    popUpGen("Error",msg[lang]);
     return false;
   }
   const email = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   let f = document.forms["contactUs"]["Email"].value;
   if (!email.test(f)) {
     msg = ["Invalid e-mail address.","Dirección de correo electrónico inválida"];
-    alert(msg[lang]);
+    popUpGen("Error",msg[lang]);
     return false;
   } 
   return true;
@@ -48,7 +48,7 @@ $('.contact1-form').on('submit',function(e){
     const errorEng = "Something went wrong. Please try again";
     const errorEsp = "Había error. Por favor trate de nuevo";
     var mailUrl;
-    if (lang < 2) mailUrl = ownerMail; // change this to ownerMail when released
+    if (lang < 2) mailUrl = devMail; // change this to ownerMail when released
     else mailUrl = devMail;
     $.ajax({
       url: mailUrl,
@@ -59,18 +59,18 @@ $('.contact1-form').on('submit',function(e){
         if(response.result == "success") {
           $('.contact1-form')[0].reset();
           msg = ["Thank you for contacting us.","Gracias por contactarnos."];
-          alert(msg[lang]);
+          popUpGen(msg[lang],"");
           return true;
         }
         else {
           msg = [errorEng,errorEsp];
-          alert(msg[lang]);
+          popUpGen(msg[lang],"");
           console.log(response.error);
         }
       },
       error: function() {
         msg = [errorEng,errorEsp];
-        alert(msg[lang]);
+        popUpGen(msg[lang],"");
       }
     })
   }
--- script.js
+++ script.js
@@ -173,3 +173,29 @@ function randomSplash() {
   }
   return retText;
 }
+
+function popUpGen(title, desc) {
+  if($('#popUp').length == 1) popUp.remove();
+  
+  body = document.getElementsByTagName("body")[0];
+  popUp = document.createElement("div");
+  popUp.id = "popUp";
+
+  popUpTitle = document.createElement("div");
+  popUpTitle.innerHTML = title;
+  popUpTitle.id = "popUpTitle";
+
+  popUpDesc = document.createElement("div");
+  popUpDesc.innerHTML = desc;
+  popUpDesc.id = "popUpDesc";
+
+  close = document.createElement("button");
+  close.setAttribute("onClick","popUp.remove()");
+  close.innerHTML = "Close";
+
+  popUp.appendChild(popUpTitle);
+  popUp.appendChild(popUpDesc);
+  popUp.appendChild(close);
+  
+  body.appendChild(popUp);
+}
